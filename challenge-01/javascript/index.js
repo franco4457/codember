1774ed5d1e96a6820c387cf9771653c4918ec54a
Franco de Desarrollo <francoegi01@gmail.com>
2023-11-04 13:06:30 -0300

    feat: :zap: challenge 01 js

---
+++ challenge-01/javascript/index.js
@@ -0,0 +1,18 @@
+import { createRequire } from 'node:module'
+
+const require = createRequire(import.meta.url)
+
+const { data } = require('../data.json')
+
+export function decript(message) {
+  const arrMsg = message.split(' ')
+  const msgDic = {}
+  arrMsg.forEach((word) => {
+    word = word.toLowerCase()
+    msgDic.hasOwnProperty(word) ? msgDic[word]++ : (msgDic[word] = 1)
+  })
+  const entries = Object.entries(msgDic)
+  return entries.flat(2).join('')
+}
+
+console.log(decript(data))
