8befa2877e5d61b0abd552735c5169916d922df1
Franco de Desarrollo <francoegi01@gmail.com>
2023-11-12 12:09:19 -0300

    feat(ts): solution challengue-02

---
+++ challenge-02/typescript/index.ts
@@ -0,0 +1,22 @@
+import { data } from '../data.json'
+
+enum Operations {
+  add = '#',
+  sub = '@',
+  mul = '*',
+  print = '&'
+}
+
+function decript(input: string) {
+  let result: string = ''
+  let current: number = 0
+  input.split('').forEach((char) => {
+    if (char === Operations.add) current++
+    if (char === Operations.sub) current--
+    if (char === Operations.mul) current *= current
+    if (char === Operations.print) result += current
+  })
+  return result
+}
+
+console.log(decript(data))
