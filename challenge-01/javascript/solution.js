cbd15af7e69d1bfa25cbe79457fadf239f191002
Franco de Desarrollo <francoegi01@gmail.com>
2023-11-14 20:54:49 -0300

    fix: import solution

--- challenge-01/javascript/solution.js
+++ challenge-01/javascript/solution.js
@@ -1,5 +1,5 @@
 import { createRequire } from 'node:module'
-import { decript3 } from '.'
+import { decript3 } from './index.js'
 const { data } = createRequire(import.meta.url)('../data.json')
 
 console.log(decript3(data))
