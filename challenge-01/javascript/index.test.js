301371946644f6d2b9a966bf416b34ea11560a16
Franco de Desarrollo <francoegi01@gmail.com>
2023-11-13 21:49:52 -0300

    test(js): decript3

--- challenge-01/javascript/index.test.js
+++ challenge-01/javascript/index.test.js
@@ -1,7 +1,7 @@
 import { createRequire } from 'node:module'
 const { data, solution } = createRequire(import.meta.url)('../data.json')
 import { expect, it, describe } from 'vitest'
-import { decript, decript2 } from './index.js'
+import { decript, decript2, decript3 } from './index.js'
 
 describe('Challenge 01 - JavaScript', () => {
   it('Example - 01', () => {
@@ -9,27 +9,32 @@ describe('Challenge 01 - JavaScript', () => {
     const result = 'llaves2casa3'
     expect(decript(msg)).toBe(result)
     expect(decript2(msg)).toBe(result)
+    expect(decript3(msg)).toBe(result)
   })
   it('Example - 02', () => {
     const msg = 'taza ta za taza'
     const result = 'taza2ta1za1'
     expect(decript(msg)).toBe(result)
     expect(decript2(msg)).toBe(result)
+    expect(decript3(msg)).toBe(result)
   })
   it('Example - 03', () => {
     const msg = 'casas casa casasas'
     const result = 'casas1casa1casasas1'
     expect(decript(msg)).toBe(result)
     expect(decript2(msg)).toBe(result)
+    expect(decript3(msg)).toBe(result)
   })
   it('Example - 04', () => {
     const msg = 'gato perro perro coche Gato peRRo sol'
     const result = 'gato2perro3coche1sol1'
     expect(decript(msg)).toBe(result)
     expect(decript2(msg)).toBe(result)
+    expect(decript3(msg)).toBe(result)
   })
   it('CHALLENGE', () => {
     expect(decript(data)).toBe(solution)
     expect(decript2(data)).toBe(solution)
+    expect(decript3(data)).toBe(solution)
   })
 })
