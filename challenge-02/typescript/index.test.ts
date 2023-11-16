1f80bac46940c786c6a80ffc14ad0ab340d3117f
Franco de Desarrollo <francoegi01@gmail.com>
2023-11-16 20:15:42 -0300

    test(ts): :sparkles: c2 compiler v3

--- challenge-02/typescript/index.test.ts
+++ challenge-02/typescript/index.test.ts
@@ -1,20 +1,25 @@
-import { createRequire } from 'node:module'
 import { data, solution } from '../data.json'
 import { expect, it, describe } from 'vitest'
-import { compiler } from './index'
+import { compiler, compiler2, compiler3 } from './index'
 
 describe('Challenge 02 - JavaScript', () => {
   it('Example - 01', () => {
     const msg = '##*&'
     const result = '4'
     expect(compiler(msg)).toBe(result)
+    expect(compiler2(msg)).toBe(result)
+    expect(compiler3(msg)).toBe(result)
   })
   it('Example - 02', () => {
     const msg = '&##&*&@&'
     const result = '0243'
     expect(compiler(msg)).toBe(result)
+    expect(compiler2(msg)).toBe(result)
+    expect(compiler3(msg)).toBe(result)
   })
   it('CHALLENGE', () => {
     expect(compiler(data)).toBe(solution)
+    expect(compiler2(data)).toBe(solution)
+    expect(compiler3(data)).toBe(solution)
   })
 })
