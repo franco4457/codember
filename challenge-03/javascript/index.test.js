c5bf2330756f41156e229ede9e561ecddb5db2a7
Franco de Desarrollo <francoegi01@gmail.com>
2023-11-21 22:25:03 -0300

    test(js): :white_check_mark: c3 initial tests

---
+++ challenge-03/javascript/index.test.js
@@ -0,0 +1,18 @@
+import { createRequire } from 'node:module'
+const { data, solution } = createRequire(import.meta.url)('../data.json')
+import { expect, it, describe } from 'vitest'
+import { decoder } from './index.js'
+
+describe('Challenge 03 - JavaScript', () => {
+  it('Example - 01', () => {
+    const list = `2-4 f: fgfff\n4-6 z: zzzsg\n1-6 h: hhhhhh`
+    expect(decoder(list, 0)).toBe('zzzsg')
+  })
+  it('Example - 02', () => {
+    const list = `1-6 y: yyyqyyfyy\n1-2 s: gfzsg\n2-5 k: pkajsdmfxcgrn\n1-6 f: ffffff`
+    expect(decoder(list, 1)).toBe('pkajsdmfxcgrn')
+  })
+  it('CHALLENGE', () => {
+    expect(decoder(data, 41)).toBe(solution)
+  })
+})
