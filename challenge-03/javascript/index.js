69c87522d589191de39f520ad9f24e1ee109d225
Franco de Desarrollo <francoegi01@gmail.com>
2023-11-22 19:51:44 -0300

    feat(js): :sparkles: c3 decoder2 & 3

--- challenge-03/javascript/index.js
+++ challenge-03/javascript/index.js
@@ -14,3 +14,58 @@ export function decoder(passLists, idxPass) {
   })
   return invalids.at(idxPass)
 }
+
+export function decoder2(passLists, idxPass) {
+  const listArr = passLists.split('\n')
+  const invalids = []
+  for (let i = 0; i < listArr.length; i++) {
+    if (invalids.at(idxPass)) return invalids.at(idxPass)
+    const [policy, pass] = listArr[i].split(': ')
+    const [minMax, key] = policy.split(' ')
+    const [min, max] = minMax.split('-')
+    let count = 0
+    pass.split('').forEach((char) => {
+      if (char === key) count++
+    })
+    if (count < min || count > max) invalids.push(pass)
+  }
+  return undefined
+}
+
+export function decoder3(passLists, idxPass) {
+  let min = passLists[0]
+  let max = null
+  let key = null
+  let pass = null
+  let count = 0
+  const invalids = []
+  for (let i = 0; i < passLists.length; i++) {
+    const char = passLists[i]
+    if (char === ':') {
+      key = passLists[i - 1]
+      i++
+      continue
+    }
+    if (char === '-') {
+      max = passLists[i + 1]
+      continue
+    }
+
+    if (key === char) count++
+    if (char === '\n') {
+      if (count < Number(min) || count > Number(max)) {
+        invalids.push(pass)
+        if (invalids.at(idxPass)) return invalids.at(idxPass)
+      }
+      count = 0
+      min = passLists[i + 1]
+      max = null
+      key = null
+      pass = null
+      continue
+    }
+    if (max == null && isNaN(+char)) min.concat(char)
+    if (isNaN(+char) && key != null) pass == null ? (pass = char) : (pass += char)
+  }
+  return undefined
+}
