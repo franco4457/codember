2ed6bcc39b06e102e57bcd5397ff1def5186900e
Franco de Desarrollo <francoegi01@gmail.com>
2023-11-30 21:17:09 -0300

    feat(ts): :sparkles: c5 add solution 2 & 3

--- challenge-05/typescript/index.ts
+++ challenge-05/typescript/index.ts
@@ -15,3 +15,52 @@ export const validateUsers = (users: string[]) => {
     return (acc += curr.split(',')[1][0])
   }, '')
 }
+
+export const validateUsers2 = (users: string[]) => {
+  let res = ''
+  for (const user of users) {
+    const [id, username, email, age, location] = user.split(',')
+    if (!id || !isAlfa.test(id)) {
+      res += username[0]
+      continue
+    }
+    if (!username || !isAlfa.test(username)) {
+      res += username[0]
+      continue
+    }
+    if (!email || !isValidEmail.test(email)) {
+      res += username[0]
+      continue
+    }
+    if (age.length !== 0 && isNaN(Number(age))) {
+      res += username[0]
+      continue
+    }
+    if (location.length !== 0 && !isNaN(Number(location))) {
+      res += username[0]
+      continue
+    }
+  }
+  return res
+}
+
+export const validateUsers3 = (users: string[]) => {
+  let res = ''
+  const DICC: Record<number, (val: string) => boolean> = {
+    0: (id: string) => !id || !isAlfa.test(id),
+    1: (username: string) => !username || !isAlfa.test(username),
+    2: (email: string) => !email || !isValidEmail.test(email),
+    3: (age: string) => age.length !== 0 && isNaN(Number(age)),
+    4: (location: string) => location.length !== 0 && !isNaN(Number(location))
+  }
+  for (const user of users) {
+    const data = user.split(',')
+    for (let i = 0; i < data.length; i++) {
+      if (DICC[i](data[i])) {
+        res += data[1][0]
+        break
+      }
+    }
+  }
+  return res
+}
--- challenge-05/typescript/solution.ts
+++ challenge-05/typescript/solution.ts
@@ -1,6 +1,6 @@
-import { validateUsers } from '.'
+import { validateUsers2 } from '.'
 import { data } from '../data.json'
 
 const dataToArray = data.split('\n')
 
-console.log(validateUsers(dataToArray))
+console.log(validateUsers2(dataToArray))
